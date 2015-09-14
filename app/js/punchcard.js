499a82f6dcd024d46b4396dbf76e5329c2759d76
Victor Mejia <victor.a.mejia@gmail.com>
2015-09-14 16:06:00 -0700

    prettify scales

--- app/js/punchcard.js
+++ app/js/punchcard.js
@@ -11,11 +11,12 @@
   function visualize() {
     // 1. Setup
     var el = d3.select('#punchcard'),
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
+        margin = {top: 20, right: 20, bottom: 30, left: 80},
         elWidth = parseInt(el.style('width'), 10),
         elHeight = parseInt(el.style('height'), 10),
         width = elWidth - margin.left - margin.right,
-        height = elHeight - margin.top - margin.bottom;
+        height = elHeight - margin.top - margin.bottom,
+        padding = 20;
 
     var svg = el.append('svg')
         .attr('width', elWidth)
@@ -38,8 +39,6 @@
       return d[2];
     });
 
-    console.log(maxR);
-
     var r = d3.scale.linear()
       .domain([0, maxR])
       .range([0, 12])
@@ -48,12 +47,20 @@
     var xAxis = d3.svg.axis()
       .scale(x)
       .orient('bottom')
-      .ticks(24);
+      .ticks(24)
+      .tickFormat(function (d, i) {
+        var suffix = (d > 12) ? 'p' : 'a';
+        var time = (d%12 === 0) ? 12 : d%12;
+        return time + suffix;
+      })
 
     var yAxis = d3.svg.axis()
       .scale(y)
       .orient('left')
-      .ticks(7);
+      .ticks(7)
+      .tickFormat(function (d, i) {
+        return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][d];
+      })
 
     svg.append('g')
       .attr('class', 'axis')
@@ -73,13 +80,12 @@
           return x(d[1]);
         })
         .attr('cy', function(d) {
-          return y(d[0])
+          return y(d[0]);
         })
         .attr('r', 0)
         .transition()
         .duration(1000)
         .attr('r', function(d) {
-          console.log(r(d[2]), Math.ceil(r(d[2])));
           var radius = Math.ceil(r(d[2]));
           return radius > 0 ? Math.ceil(radius * 1.5) : 0;
         });
