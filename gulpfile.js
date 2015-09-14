bf5724e34235d7ad5486894c9d3ed237c03844f3
Victor Mejia <victor.a.mejia@gmail.com>
2015-09-14 09:30:34 -0700

    adding deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
+var ghPages     = require('gulp-gh-pages');
 
 // use default task to launch Browsersync and watch JS files
 gulp.task('serve',  function () {
@@ -16,3 +17,9 @@ gulp.task('serve',  function () {
     // all browsers reload after tasks are complete.
     gulp.watch("app/**/*").on("change", browserSync.reload);
 });
+
+
+gulp.task('deploy', function() {
+  return gulp.src('./app/**/*')
+    .pipe(ghPages());
+})
